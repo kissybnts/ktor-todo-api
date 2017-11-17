0dee6c624a87f5c8409e7843064860c6b25dc88c
Masataka <fstt.fwmym.f15@gmail.com>
2017-11-17 21:00:56 +0900

    First commit

---
+++ src/main/kotlin/com/kissybnts/Application.kt
@@ -0,0 +1,50 @@
+package com.kissybnts
+
+import io.ktor.application.Application
+import io.ktor.application.call
+import io.ktor.application.install
+import io.ktor.features.CallLogging
+import io.ktor.features.ContentNegotiation
+import io.ktor.features.DefaultHeaders
+import io.ktor.gson.gson
+import io.ktor.http.HttpStatusCode
+import io.ktor.response.respond
+import io.ktor.routing.Routing
+import io.ktor.routing.get
+import org.jetbrains.exposed.dao.EntityID
+import org.jetbrains.exposed.dao.IntEntity
+import org.jetbrains.exposed.dao.IntEntityClass
+import org.jetbrains.exposed.dao.IntIdTable
+import org.jetbrains.exposed.sql.Database
+import org.jetbrains.exposed.sql.transactions.transaction
+import java.text.DateFormat
+
+fun Application.main() {
+    Database.apply {
+        val host = System.getenv("DB_HOST")?: "127.0.0.1"
+        val port = System.getenv("DB_PORT")?: "3306"
+        val name = System.getenv("DB_NAME")?: "ktor-todo"
+        val user = System.getenv("DB_USER")?: "root"
+        val password = System.getenv("DB_PASS")?: ""
+        val driver = environment.config.config("database").property("driver").getString()
+        connect("jdbc:mysql://$host:$port/$name?useSSL=false", driver, user, password)
+    }
+    install(DefaultHeaders)
+    install(CallLogging)
+    install(ContentNegotiation) {
+        gson {
+            setDateFormat(DateFormat.LONG)
+            setPrettyPrinting()
+        }
+    }
+    install(Routing) {
+        get("/") {
+            call.respond(HttpStatusCode.OK, "Hello from Ktor!")
+        }
+        get("/items") {
+            call.respond(HttpStatusCode.OK, Item("This is a key", "This is a value"))
+        }
+    }
+}
+
+data class Item(val key: String, val value: String)
