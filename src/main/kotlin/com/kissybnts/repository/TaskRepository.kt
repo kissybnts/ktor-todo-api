61dbe3d03c369d4f64afaf55109d2c7de500a2cc
Masataka Kishida <fstt.fwmym.f15@gmail.com>
2017-11-19 19:15:35 +0900

    Implement complete method in TaskRepository object

--- src/main/kotlin/com/kissybnts/repository/TaskRepository.kt
+++ src/main/kotlin/com/kissybnts/repository/TaskRepository.kt
@@ -2,10 +2,12 @@ package com.kissybnts.repository
 
 import com.kissybnts.table.ProjectTable
 import com.kissybnts.table.TaskTable
+import com.mysql.cj.api.xdevapi.UpdateStatement
 import org.jetbrains.exposed.sql.ResultRow
 import org.jetbrains.exposed.sql.insert
 import org.jetbrains.exposed.sql.select
 import org.jetbrains.exposed.sql.transactions.transaction
+import org.jetbrains.exposed.sql.update
 import org.joda.time.DateTime
 
 data class Task(val id: Int, val projectId: Int, val name: String, val description: String, val dueDate: String, val isCompleted: Boolean = false) {
@@ -39,7 +41,7 @@ object TaskRepository {
 
     fun insert(newTask: NewTask): Task = transaction { insertWithoutTransaction(newTask) }
 
-    fun insertWithoutTransaction(newTask: NewTask): Task {
+    private fun insertWithoutTransaction(newTask: NewTask): Task {
         val statement = TaskTable.insert {
             it[TaskTable.projectId] = newTask.projectId
             it[TaskTable.name] = newTask.name
@@ -48,5 +50,24 @@ object TaskRepository {
         }
         val id = statement.generatedKey?.toInt() ?: throw IllegalStateException("Generated id is null")
         return Task(id, newTask.projectId, newTask.name, newTask.description, newTask.dueDate.toString("yyyy/MM/dd"))
+    }
+
+    fun complete(id: Int): Int = transaction { completeWithoutTransaction(id) }
+
+    private fun completeWithoutTransaction(id: Int): Int {
+        return TaskTable.update({ TaskTable.id.eq(id) }) {
+            it[isCompleted] = true
+            it[updatedAt] = DateTime()
+        }
+    }
+
+    fun complete(ids: List<Int>) = transaction { completeWithoutTransaction(ids) }
+
+    private fun completeWithoutTransaction(ids: List<Int>): Int {
+        val now = DateTime()
+        return TaskTable.update({ TaskTable.id.inList(ids) }) {
+            it[TaskTable.isCompleted] = true
+            it[TaskTable.updatedAt] = now
+        }
     }
 }
