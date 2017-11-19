83d0d18b14926beb6886f99a71e9553bcd03d5b0
Masataka Kishida <fstt.fwmym.f15@gmail.com>
2017-11-19 11:21:01 +0900

    Change to use location for structured routing purpose

--- src/main/kotlin/com/kissybnts/route/ProjectRoute.kt
+++ src/main/kotlin/com/kissybnts/route/ProjectRoute.kt
@@ -1,39 +1,67 @@
 package com.kissybnts.route
 
 import com.kissybnts.repository.ProjectRepository
+import io.ktor.application.ApplicationCall
 import io.ktor.application.call
+import io.ktor.http.HttpMethod
 import io.ktor.http.HttpStatusCode
-import io.ktor.locations.get
+import io.ktor.locations.location
 import io.ktor.request.receiveOrNull
 import io.ktor.response.respond
 import io.ktor.routing.Route
-import io.ktor.routing.post
+import io.ktor.routing.method
 
 data class NewProject(val name: String, val description: String)
 
 fun Route.projects() {
-    get<Index> {
-        // TODO change to use the user id of which logged in user
-        val all = ProjectRepository.selectAll(1)
-        call.respond(all)
-    }
-
-    post {
-        val request = call.receiveOrNull<NewProject>()
-        if (request == null) {
-            call.respond(HttpStatusCode.BadRequest)
-            return@post
+    location<Index> {
+        method(HttpMethod.Get) {
+            handle {
+                // TODO change to use the user id of which logged in user
+                val all = ProjectRepository.selectAll(1)
+                call.respond(all)
+            }
         }
-        val project = ProjectRepository.insert(request)
-        call.respond(HttpStatusCode.OK, project)
-    }
 
-    get<ResourceId> {
-        val project = ProjectRepository.select(it.id)
-        if (project != null) {
-            call.respond(project)
-        } else {
-            call.respond(HttpStatusCode.NotFound)
+        method(HttpMethod.Post) {
+            handle {
+                val request = call.receiveOrNull<NewProject>()
+                if (request == null) {
+                    call.badRequest()
+                    return@handle
+                }
+                val project = ProjectRepository.insert(request)
+                call.respond(project)
+            }
         }
     }
+
+    location<ResourceId> {
+        method(HttpMethod.Get) {
+            handle {
+                val id = call.parameters["id"]?.toIntOrNull()
+                if (id == null) {
+                    call.badRequest()
+                    return@handle
+                }
+
+                val project = ProjectRepository.select(id)
+                if (project != null) {
+                    call.respond(project)
+                } else {
+                    call.notFound()
+                }
+
+            }
+        }
+    }
+
+}
+
+suspend fun ApplicationCall.notFound() {
+    respond(HttpStatusCode.NotFound)
+}
+
+suspend fun ApplicationCall.badRequest() {
+    respond(HttpStatusCode.BadRequest)
 }
