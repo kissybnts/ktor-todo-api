1e4288df36411a3ede34af78cbf08fb0138e4d3a
Masataka Kishida <fstt.fwmym.f15@gmail.com>
2017-12-01 15:15:32 +0900

    Move set up method to new file named SetUpExtension

--- src/main/kotlin/com/kissybnts/Application.kt
+++ src/main/kotlin/com/kissybnts/Application.kt
@@ -4,13 +4,8 @@ import com.fasterxml.jackson.databind.PropertyNamingStrategy
 import com.fasterxml.jackson.databind.SerializationFeature
 import com.fasterxml.jackson.datatype.joda.JodaModule
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule
-import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateDeserializer
-import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateTimeDeserializer
-import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateSerializer
-import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateTimeSerializer
-import com.kissybnts.app.EnvironmentVariableKeys
-import com.kissybnts.app.FormatConstants
 import com.kissybnts.extension.ok
+import com.kissybnts.extension.setUp
 import com.kissybnts.route.Index
 import com.kissybnts.route.login
 import com.kissybnts.route.projects
@@ -18,7 +13,6 @@ import com.kissybnts.route.tasks
 import io.ktor.application.*
 import io.ktor.client.HttpClient
 import io.ktor.client.backend.apache.ApacheBackend
-import io.ktor.config.ApplicationConfig
 import io.ktor.features.CallLogging
 import io.ktor.features.ContentNegotiation
 import io.ktor.features.DefaultHeaders
@@ -30,9 +24,6 @@ import io.ktor.response.respond
 import io.ktor.routing.Routing
 import io.ktor.routing.route
 import org.jetbrains.exposed.sql.Database
-import java.time.LocalDate
-import java.time.LocalDateTime
-import java.time.format.DateTimeFormatter
 
 fun Application.main() {
     val databaseConfig = environment.config.config("database")
@@ -47,12 +38,7 @@ fun Application.main() {
             propertyNamingStrategy = PropertyNamingStrategy.SNAKE_CASE
             // TODO remove this, and then add JavaTime module and define default serializer/deserializer
             registerModule(JodaModule())
-            registerModule(JavaTimeModule().apply {
-                addSerializer(LocalDateTime::class.java, LocalDateTimeSerializer(DateTimeFormatter.ofPattern(FormatConstants.DATE_TIME_FORMAT)))
-                addDeserializer(LocalDateTime::class.java, LocalDateTimeDeserializer(DateTimeFormatter.ofPattern(FormatConstants.DATE_TIME_FORMAT)))
-                addSerializer(LocalDate::class.java, LocalDateSerializer(DateTimeFormatter.ofPattern(FormatConstants.DATE_FORMAT)))
-                addDeserializer(LocalDate::class.java, LocalDateDeserializer(DateTimeFormatter.ofPattern(FormatConstants.DATE_FORMAT)))
-            })
+            registerModule(JavaTimeModule().setUp())
         }
     }
 
@@ -91,18 +77,3 @@ fun Application.main() {
 }
 
 fun getEnv(name: String, default: String): String = System.getenv(name)?: default
-
-/**
- * set up database config with `ApplicationConfig` and environment variables.
- */
-private fun Database.Companion.setUp(databaseConfig: ApplicationConfig) {
-    apply {
-        val host = getEnv(EnvironmentVariableKeys.Gradle.DB_HOST, "127.0.0.1")
-        val port = getEnv(EnvironmentVariableKeys.Gradle.DB_PORT, "3306")
-        val name = getEnv(EnvironmentVariableKeys.Gradle.DB_NAME, "ktor-todo")
-        val user = getEnv(EnvironmentVariableKeys.Gradle.DB_USER, "root")
-        val password = getEnv(EnvironmentVariableKeys.Gradle.DB_PASS, "")
-        val driver = databaseConfig.property("driver").getString()
-        connect("jdbc:mysql://$host:$port/$name?useSSL=false", driver, user, password)
-    }
-}
---
+++ src/main/kotlin/com/kissybnts/extension/SetUpExtension.kt
@@ -0,0 +1,38 @@
+package com.kissybnts.extension
+
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule
+import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateDeserializer
+import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateTimeDeserializer
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateSerializer
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateTimeSerializer
+import com.kissybnts.app.EnvironmentVariableKeys
+import com.kissybnts.app.FormatConstants
+import com.kissybnts.getEnv
+import io.ktor.config.ApplicationConfig
+import org.jetbrains.exposed.sql.Database
+import java.time.LocalDate
+import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
+
+/**
+ * set up database config with `ApplicationConfig` and environment variables.
+ */
+internal fun Database.Companion.setUp(databaseConfig: ApplicationConfig) {
+    apply {
+        val host = getEnv(EnvironmentVariableKeys.Gradle.DB_HOST, "127.0.0.1")
+        val port = getEnv(EnvironmentVariableKeys.Gradle.DB_PORT, "3306")
+        val name = getEnv(EnvironmentVariableKeys.Gradle.DB_NAME, "ktor-todo")
+        val user = getEnv(EnvironmentVariableKeys.Gradle.DB_USER, "root")
+        val password = getEnv(EnvironmentVariableKeys.Gradle.DB_PASS, "")
+        val driver = databaseConfig.property("driver").getString()
+        connect("jdbc:mysql://$host:$port/$name?useSSL=false", driver, user, password)
+    }
+}
+
+internal fun JavaTimeModule.setUp(): JavaTimeModule {
+    addSerializer(LocalDateTime::class.java, LocalDateTimeSerializer(DateTimeFormatter.ofPattern(FormatConstants.DATE_TIME_FORMAT)))
+    addDeserializer(LocalDateTime::class.java, LocalDateTimeDeserializer(DateTimeFormatter.ofPattern(FormatConstants.DATE_TIME_FORMAT)))
+    addSerializer(LocalDate::class.java, LocalDateSerializer(DateTimeFormatter.ofPattern(FormatConstants.DATE_FORMAT)))
+    addDeserializer(LocalDate::class.java, LocalDateDeserializer(DateTimeFormatter.ofPattern(FormatConstants.DATE_FORMAT)))
+    return this
+}
