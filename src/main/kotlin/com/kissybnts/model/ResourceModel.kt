d64b9fb848eed7fdaad8842d9fb2f5590d4a7773
Masataka Kishida <fstt.fwmym.f15@gmail.com>
2017-12-01 12:21:06 +0900

    Add json ignore annotation in order not to return unnecessary value

--- src/main/kotlin/com/kissybnts/model/ResourceModel.kt
+++ src/main/kotlin/com/kissybnts/model/ResourceModel.kt
@@ -1,5 +1,6 @@
 package com.kissybnts.model
 
+import com.fasterxml.jackson.annotation.JsonIgnore
 import com.kissybnts.extension.toJavaLocalDate
 import com.kissybnts.extension.toJavaLocalDateTime
 import com.kissybnts.table.AuthProvider
@@ -7,7 +8,6 @@ import com.kissybnts.table.ProjectTable
 import com.kissybnts.table.TaskTable
 import com.kissybnts.table.UserTable
 import org.jetbrains.exposed.sql.ResultRow
-import org.joda.time.DateTime
 import java.time.LocalDate
 import java.time.LocalDateTime
 
@@ -17,7 +17,9 @@ data class UserModel(val id: Int,
                      val providerType: AuthProvider,
                      val providerCode: String,
                      val providerId: Int,
+                     @JsonIgnore
                      val createdAt: LocalDateTime,
+                     @JsonIgnore
                      val updatedAt: LocalDateTime) {
     constructor(resultRow: ResultRow): this(
             resultRow[UserTable.id].value,
@@ -35,7 +37,9 @@ data class ProjectModel(val id: Int,
                         val userId: Int,
                         val name: String,
                         val description: String,
+                        @JsonIgnore
                         val createdAt: LocalDateTime,
+                        @JsonIgnore
                         val updatedAt: LocalDateTime) {
     constructor(resultRow: ResultRow): this(
             resultRow[ProjectTable.id].value,
@@ -53,7 +57,9 @@ data class TaskModel(val id: Int,
                      val description: String,
                      val dueDate: LocalDate,
                      val isCompleted: Boolean,
+                     @JsonIgnore
                      val createdAt: LocalDateTime,
+                     @JsonIgnore
                      val updatedAt: LocalDateTime) {
     constructor(resultRow: ResultRow): this(
             resultRow[TaskTable.id].value,
