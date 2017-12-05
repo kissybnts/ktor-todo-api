6ccfec036fb8e340d7846b5a4eeea98b3667a6d3
Masataka Kishida <fstt.fwmym.f15@gmail.com>
2017-12-05 23:21:58 +0900

    Add extension of Cipher in order to encrypt and verify password

--- src/main/kotlin/kissybnts/ktor_todo/extension/CipherExtension.kt
+++ src/main/kotlin/kissybnts/ktor_todo/extension/CipherExtension.kt
@@ -1,2 +1,12 @@
 package kissybnts.ktor_todo.extension
 
+import java.util.*
+import javax.crypto.Cipher
+
+fun Cipher.passwordEncrypt(value: String): String {
+    return String(Base64.getEncoder().encode(doFinal(value.toByteArray())))
+}
+
+fun Cipher.isCorrectPassword(requestPassword: String, encryptedPassword: String): Boolean {
+    return String(doFinal(Base64.getDecoder().decode(encryptedPassword.toByteArray()))) == requestPassword
+}
