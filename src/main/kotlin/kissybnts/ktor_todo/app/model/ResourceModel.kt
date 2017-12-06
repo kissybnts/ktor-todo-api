44cd5f54d8656d9ee2cdf17baee1eb3370fb7840
Masataka Kishida <fstt.fwmym.f15@gmail.com>
2017-12-07 00:01:50 +0900

    Add models

--- src/main/kotlin/kissybnts/ktor_todo/app/model/ResourceModel.kt
+++ src/main/kotlin/kissybnts/ktor_todo/app/model/ResourceModel.kt
@@ -4,9 +4,8 @@ import com.fasterxml.jackson.annotation.JsonIgnore
 import kissybnts.ktor_todo.extension.toJavaLocalDate
 import kissybnts.ktor_todo.extension.toJavaLocalDateTime
 import kissybnts.ktor_todo.app.enumeration.AuthProvider
-import kissybnts.ktor_todo.app.table.ProjectTable
-import kissybnts.ktor_todo.app.table.TaskTable
-import kissybnts.ktor_todo.app.table.UserTable
+import kissybnts.ktor_todo.app.enumeration.AuthType
+import kissybnts.ktor_todo.app.table.*
 import org.jetbrains.exposed.sql.ResultRow
 import java.time.LocalDate
 import java.time.LocalDateTime
@@ -14,6 +13,7 @@ import java.time.LocalDateTime
 data class UserModel(val id: Int,
                      val name: String,
                      val imageUrl: String,
+                     val authType: AuthType,
                      val providerType: AuthProvider,
                      val providerCode: String,
                      val providerId: Int,
@@ -25,11 +25,45 @@ data class UserModel(val id: Int,
             resultRow[UserTable.id],
             resultRow[UserTable.name],
             resultRow[UserTable.imageUrl],
+            resultRow[UserTable.authType],
             resultRow[UserTable.providerType],
             resultRow[UserTable.providerCode],
             resultRow[UserTable.providerId],
             resultRow[UserTable.createdAt].toJavaLocalDateTime(),
             resultRow[UserTable.updatedAt].toJavaLocalDateTime()
+    )
+}
+
+sealed class AuthCredentialModel
+
+data class OAuthCredentialModel(val providerType: AuthProvider,
+                                val providerCode: String,
+                                val projectId: Int,
+                                @JsonIgnore
+                                val createdAt: LocalDateTime,
+                                @JsonIgnore
+                                val updatedAt: LocalDateTime) : AuthCredentialModel() {
+    constructor(resultRow: ResultRow): this(
+            resultRow[OAuthCredentialTable.providerType],
+            resultRow[OAuthCredentialTable.providerCode],
+            resultRow[OAuthCredentialTable.providerId],
+            resultRow[OAuthCredentialTable.createdAt].toJavaLocalDateTime(),
+            resultRow[OAuthCredentialTable.updatedAt].toJavaLocalDateTime()
+    )
+}
+
+data class EmailCredentialModel(val email: String,
+                                @JsonIgnore
+                                val password: String,
+                                @JsonIgnore
+                                val createdAt: LocalDateTime,
+                                @JsonIgnore
+                                val updatedAt: LocalDateTime) : AuthCredentialModel() {
+    constructor(resultRow: ResultRow): this(
+            resultRow[EmailCredentialTable.email],
+            resultRow[EmailCredentialTable.password],
+            resultRow[EmailCredentialTable.createdAt].toJavaLocalDateTime(),
+            resultRow[EmailCredentialTable.updatedAt].toJavaLocalDateTime()
     )
 }
 
