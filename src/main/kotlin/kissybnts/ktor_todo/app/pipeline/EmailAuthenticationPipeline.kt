cf31ef85c5189eeddeaf3a2103268bbd4a210ae0
Masataka Kishida <fstt.fwmym.f15@gmail.com>
2017-12-07 21:14:19 +0900

    Remove EmailAuthenticationPipeline

--- src/main/kotlin/kissybnts/ktor_todo/app/pipeline/EmailAuthenticationPipeline.kt
+++
@@ -1,9 +0,0 @@
-package kissybnts.ktor_todo.app.pipeline
-
-import io.ktor.auth.AuthenticationPipeline
-
-fun AuthenticationPipeline.emailAuthentication() {
-    intercept(AuthenticationPipeline.RequestAuthentication) {
-        TODO()
-    }
-}
